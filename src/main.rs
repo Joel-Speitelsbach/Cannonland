6516f0a40550341e6a0cdc016a5d92ff4893ea0f
Joel Speitelsbach <joel.speitelsbach@gmail.com>
2024-10-03 00:35:23 +0200

    start into main menu without cli parameter

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,34 @@ mod sound;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() < 2 {
-        eprintln!(
-            "Usage:\
-            \n     cargo run <module name> <args>"
-        );
-        std::process::exit(0);
-    }
-
-    match args[1].as_str() {
-        "particle_test" => battlefield::grid::particle_test::run(),
-        "present" => serverless_client::run_standalone(),
-        "client" => client::run_standalone(&args[2]),
-        "server" => server::run(),
-        "program" => program::run(),
-        "sound" => sound::test(),
-        x => println!("module name {} does not exist", x),
+    if args.len() < 2 { program::run() }
+    else {
+        match args[1].as_str() {
+            "particle_test" => battlefield::grid::particle_test::run(),
+            "present" => serverless_client::run_standalone(),
+            "client" => {
+                if args.len() < 3 {
+                    eprintln!(
+                        "Usage: cargo run client [hostname]\
+                        \n       cargo run client [ip address of server]
+                        "
+                    );
+                    std::process::exit(-1);
+                }
+                client::run_standalone(&args[2])
+            },
+            "server" => server::run(),
+            "program" => program::run(),
+            "sound" => sound::test(),
+            x => {
+                println!("module name {} does not exist", x);
+                eprintln!(
+                    "Usage:\
+                    \n     cargo run [module name] [args] 
+                    "
+                );
+                std::process::exit(-1);
+            },
+        }
     }
 }
